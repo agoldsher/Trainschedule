08bc32798ca0a8fa4dec5e4f92a294696c2743a1
Blue Buffalo <anettagoldsher@gmail.com>
2018-12-03 17:14:38 -0700

    can add trains, but it is not very good at updating everything yet

--- assets/javascript/trains.js
+++ assets/javascript/trains.js
@@ -14,66 +14,109 @@
 let train = [
     {
         frequency: 60,
-        arrival: "07:00 p"
+        arrival: "06:30 a"
     },
     {
         frequency: 90,
-        arrival: "06:45 p"
+        arrival: "08:00 a"
     },
     {
-        frequency: 600,
-        arrival: "10:00 p"
+        frequency: 120,
+        arrival: "09:10 a"
     }
 ];
+// console.log(train);
+// train.push({
+//     frequency: 10,
+//     arrival: "00:30 p"
+// });
+// console.log(train);
 
-// // Updating train1 stats
-// $("#train1").find(".frequency").html(train1.frequency);
-// $("#train1").find(".arrival").html(train1.firstArrival);
-// $("#train1").find(".arrival").html(moment(train1.firstArrival, 'HH:mm').format('hh:mm a'));
-// $("#train1").find(".min-away").html(moment(train1.firstArrival, 'HH:mm').fromNow());
-
-// // Updating train2 stats
-// $("#train2").find(".frequency").html(train2.frequency);
-// $("#train2").find(".arrival").html(moment(train2.arrival, 'hh:mm a').format('hh:mm a'));
-// $("#train2").find(".min-away").html(moment(train2.arrival, 'hh:mm a').fromNow());
-// if (train2.arrival <= moment().format('hh:mm:ss a')) {
-
-//     train2.arrival = moment(train2.arrival, 'hh:mm a').add(train2.frequency, 'minutes').calendar();
-//     $("#train2").find(".arrival").html(moment(train2.arrival, 'hh:mm a').format('hh:mm a'));
-//     $("#train2").find(".min-away").html(moment(train2.arrival, 'hh:mm a').fromNow());
-// }
-// console.log(train2.arrival);
-// $("#train2").find(".arrival").html(moment(train2.arrival, 'hh:mm a').format('hh:mm a'));
-
-
-// // Updating train3 stats
-// $("#train3").find(".frequency").html(train3.frequency);
-// $("#train3").find(".arrival").html(train3.firstArrival);
-// $("#train3").find(".arrival").html(moment(train3.firstArrival, 'HH:mm').format('hh:mm a'));
-// $("#train3").find(".min-away").html(moment(train3.firstArrival, 'HH:mm').fromNow());
-
-
-function updateTrainTime(i) {
-
-    $("#train" + i).find(".frequency").html(train[i].frequency);
-    if (train[i].arrival <= moment().format('hh:mm:ss a')) {
-        console.log("if " + i);
-        train[i].arrival = moment(train[i].arrival, 'hh:mm a').add(train[i].frequency, 'minutes').calendar();
-        console.log(train[i].arrival);
-        $("#train" + i).find(".arrival").html(moment(train[i].arrival, 'hh:mm a').format('hh:mm a'));
-        $("#train" + i).find(".min-away").html(moment(train[i].arrival, 'hh:mm a').fromNow());
-    } else {
-        console.log("else " + i);
-        $("#train" + i).find(".frequency").html(train[i].frequency);
-        $("#train" + i).find(".arrival").html(moment(train[i].arrival, 'hh:mm a').format('hh:mm a'));
-        $("#train" + i).find(".min-away").html(moment(train[i].arrival, 'hh:mm a').fromNow());
+function initialTrainSchedule() {
+    for (let i = 0; i < train.length; i++) {
+        if (train[i].arrival <= moment().format('hh:mm a')) {
+            console.log("if " + i);
+            train[i].arrival = moment(train[i].arrival, 'hh:mm a').add(train[i].frequency, 'minutes').calendar();
+            console.log(train[i].arrival);
+            $("#train" + i).find(".arrival").html(moment(train[i].arrival, 'hh:mm a').format('hh:mm a'));
+            $("#train" + i).find(".min-away").html(moment(train[i].arrival, 'hh:mm a').fromNow());
+        } else {
+            console.log("else " + i);
+            $("#train" + i).find(".frequency").html(train[i].frequency);
+            $("#train" + i).find(".arrival").html(moment(train[i].arrival, 'hh:mm a').format('hh:mm a'));
+            $("#train" + i).find(".min-away").html(moment(train[i].arrival, 'hh:mm a').fromNow());
+            console.log(localStorage.getItem("train" + i + "-frequency", train[i].arrival))
+        }
     }
-    localStorage.setItem("train" + i + "-frequency", train[i].frequency);
-    localStorage.setItem("train" + i + "-arrival", train[i].arrival);
+}
+console.log(moment().format('hh:mm:ss a'));
 
+function updateTrainTime() {
+    console.log("update");
+    for (let i = 0; i < train.length; i++) {
+        console.log(localStorage.getItem("train" + i + "-arrival"));
+        if (localStorage.getItem("train" + i + "-arrival") <= moment().format('hh:mm a')) {
+            console.log("if " + i);
+            let temp = moment(localStorage.getItem("train" + i + "-arrival"), 'hh:mm a').add(localStorage.getItem("train" + i + "-frequency"), 'minutes').calendar();
+            localStorage.setItem("train" + i + "-arrival", temp);
+            $("#train" + i).find(".arrival").html(moment(localStorage.getItem("train" + i + "-arrival"), 'hh:mm a').format('hh:mm a'));
+            $("#train" + i).find(".min-away").html(moment(localStorage.getItem("train" + i + "-arrival"), 'hh:mm a').fromNow());
+        } else {
+            console.log("else " + i);
+            moment.relativeTimeThreshold('m', 1000);
+            $("#train" + i).find(".frequency").html(localStorage.getItem("train" + i + "-frequency"));
+            $("#train" + i).find(".arrival").html(moment(localStorage.getItem("train" + i + "-arrival"), 'hh:mm a').format('hh:mm a'));
+            $("#train" + i).find(".min-away").html(moment(localStorage.getItem("train" + i + "-arrival"), 'hh:mm a').fromNow());
+
+
+        }
+    }
 }
 
-for (let i = 0; i < 3; i++) {
-    updateTrainTime(i);
 
+function toBeExecutedOnFirstLoad() {
+    console.log("initial");
+    for (let i = 0; i < 3; i++) {
+        initialTrainSchedule();
+        localStorage.setItem("train" + i + "-frequency", train[i].frequency);
+        localStorage.setItem("train" + i + "-arrival", train[i].arrival);
+    }
 }
+
+if (localStorage.getItem('first') === null) {
+    toBeExecutedOnFirstLoad();
+    localStorage.setItem('first', 'nope!');
+}
+
+updateTrainTime()
+
+var i = 2
+
+$("#submit").on("click", function () {
+    i++;
+
+    var addTrainName = $("#train-name").val();
+    $("tbody").append("<tr class='text-center' id='train" + i + "'></tr>");
+    $("#train" + i).append("<td class='name'>" + addTrainName + "</td>");
+    var addTrainDestination = $("#train-destination").val();
+    $("#train" + i).append("<td class='destination'>" + addTrainDestination + "</td>");
+
+    var addTrainFrequency = $("#train-frequency").val();
+    var addTrainArrival1 = $("#train-time-1").val()
+    $("#train" + i).append("<td class='frequency'></td>");
+    $("#train" + i).append("<td class='arrival'></td>");
+    $("#train" + i).append("<td class='min-away'></td>");
+
+    localStorage.setItem("train" + i + "-frequency", addTrainFrequency);
+    localStorage.setItem("train" + i + "-arrival", addTrainArrival1);
+
+
+    train.push({
+        frequency: parseInt(addTrainFrequency),
+        arrival: addTrainArrival1
+    })
+
+    console.log(train);
+    initialTrainSchedule();
+})
+
