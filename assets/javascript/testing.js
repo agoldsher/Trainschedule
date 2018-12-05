22d1d2812b06de0a13ccb96c06de9fd1bb5f2ef9
Blue Buffalo <anettagoldsher@gmail.com>
2018-12-04 21:37:12 -0700

    added comments

--- assets/javascript/testing.js
+++ assets/javascript/testing.js
@@ -1,3 +1,4 @@
+// original trains on the pages are listed in this object
 let train = [
     {
         name: "Trenton Express",
@@ -18,21 +19,24 @@ let train = [
         arrival: "07:50 a"
     }
 ];
+// this function stores some variables into local storage to 
+// allow for other fuctions to access, including all the information
+// needed to fill out the train schedule. This fuction fills out the 
+// page the first time the user opens the page
 function toBeExecutedOnFirstLoad() {
-    console.log("initial");
     localStorage.setItem("length", 3);
     localStorage.setItem("i", 2);
     for (let i = 0; i < localStorage.getItem("length"); i++) {
-        // initialTrainSchedule();
         localStorage.setItem("train" + i + "-name", train[i].name);
         localStorage.setItem("train" + i + "-destination", train[i].destination);
         localStorage.setItem("train" + i + "-frequency", train[i].frequency);
         localStorage.setItem("train" + i + "-arrival", train[i].arrival);
-
     }
     renderPage();
     updateTrainTime();
 }
+// this checks to see if the user has already been to the page
+// and runs the appropriate script accordingly
 if (localStorage.getItem('first') === null) {
     toBeExecutedOnFirstLoad();
     localStorage.setItem('first', 'nope!');
@@ -40,31 +44,29 @@ if (localStorage.getItem('first') === null) {
     renderPage();
     updateTrainTime();
 }
-
-
+// this function loops through all the trains in local storage
+// and displays the facts about the train that are constant in the table
+// ie, name, destination, and frequency
+// length is used to keep track of how many trains there are
 function renderPage() {
     for (let i = 0; i < localStorage.getItem("length"); i++) {
         var addTrainName = localStorage.getItem("train" + i + "-name");
         $("tbody").append("<tr class='text-center' id='train" + i + "'></tr>");
         $("#train" + i).append("<td class='name'>" + addTrainName + "</td>");
-
         var addTrainDestination = localStorage.getItem("train" + i + "-destination");
         $("#train" + i).append("<td class='destination'>" + addTrainDestination + "</td>");
-
         var addTrainFrequency = localStorage.getItem("train" + i + "-frequency");
         $("#train" + i).append("<td class='frequency'>" + addTrainFrequency + "</td>");
         $("#train" + i).append("<td class='arrival'></td>");
         $("#train" + i).append("<td class='min-away'></td>");
-
-
     }
 }
+// this function updates all the changing parts of the train schedule
+// it checks to see if the time is current and runs the script accordingly
+// the arrival time and the minutes away sections are updated here
 function updateTrainTime() {
-    console.log("update");
     moment.relativeTimeThreshold('m', 1000);
     for (let i = 0; i < localStorage.getItem("length"); i++) {
-
-        console.log(localStorage.getItem("train" + i + "-arrival"));
         if ((moment(localStorage.getItem("train" + i + "-arrival"), "hh:mm a") - moment()) <= 0) {
             while ((moment(localStorage.getItem("train" + i + "-arrival"), "hh:mm a") - moment()) <= 0) {
                 console.log("if " + i);
@@ -74,37 +76,28 @@ function updateTrainTime() {
                 $("#train" + i).find(".min-away").html(moment(localStorage.getItem("train" + i + "-arrival"), 'hh:mm a').fromNow(true));
             }
         } else {
-            console.log("else " + i);
-
             $("#train" + i).find(".frequency").html(localStorage.getItem("train" + i + "-frequency"));
             $("#train" + i).find(".arrival").html(moment(localStorage.getItem("train" + i + "-arrival"), 'hh:mm a').format('hh:mm a'));
             $("#train" + i).find(".min-away").html(moment(localStorage.getItem("train" + i + "-arrival"), 'hh:mm a').fromNow(true));
-
-
         }
     }
 }
-
+// when a new train is submitted, the data is stored to local storage
+// and the page is rendered again and the train times are updated.
+// the variable i allows for the trains to be IDed properly
 $("#submit").on("click", function () {
     let i = localStorage.getItem("i");
     i++;
     localStorage.setItem("i", i);
-    var addTrainName = $("#train-name").val();
-    // $("tbody").append("<tr class='text-center' id='train" + i + "'></tr>");
-    // $("#train" + i).append("<td class='name'>" + addTrainName + "</td>");
-    var addTrainDestination = $("#train-destination").val();
-    // $("#train" + i).append("<td class='destination'>" + addTrainDestination + "</td>");
-
-    var addTrainFrequency = $("#train-frequency").val();
-    var addTrainArrival1 = $("#train-time-1").val()
+    let addTrainName = $("#train-name").val();
+    let addTrainDestination = $("#train-destination").val();
+    let addTrainFrequency = $("#train-frequency").val();
+    let addTrainArrival1 = $("#train-time-1").val()
     localStorage.setItem("train" + i + "-name", addTrainName);
     localStorage.setItem("train" + i + "-destination", addTrainDestination);
     localStorage.setItem("train" + i + "-frequency", addTrainFrequency);
     localStorage.setItem("train" + i + "-arrival", addTrainArrival1);
-
-
-
-    var length = localStorage.getItem("length");
+    let length = localStorage.getItem("length");
     length++;
     localStorage.setItem("length", length);
     console.log(train);
